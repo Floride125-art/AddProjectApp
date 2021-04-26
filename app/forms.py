0209df78082862c1cdf0c74561ab9b772417273a
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-27 01:31:30 +0200

    add fields to help users add projects name, link, and images

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class UserCreationFormm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ('project_name', 'link', 'details','image')
