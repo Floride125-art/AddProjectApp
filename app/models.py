8b31039c5774889df5b14ca622ead2eea35ab942
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-27 01:38:04 +0200

    create a model class with functions to add user profile

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/', default='default.jpg')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
+
+    def __str__(self):
+         return self.user.username
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
