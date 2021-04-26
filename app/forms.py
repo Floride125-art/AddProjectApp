2a514a0ab29da29abf0a8e7f7c8569f9a3dd0836
Floride125-art <FTuyisenge120@daviscollege.com>
2021-04-27 01:34:51 +0200

    create a class to help user update her or his profile

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Project, Profile, Rate
 class UserCreationFormm(UserCreationForm):
     class Meta:
         model = User
@@ -13,3 +14,8 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'bio', 'contact']
